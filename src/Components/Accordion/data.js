70851fe38017d64ebfbf079dadbb1c3c9e45044b
Your Name <nellie7190@gmail.com>
2024-02-08 16:09:42 -0500

    added data to data.js to be collapsed

---
+++ src/Components/Accordion/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        id : '1',
+        question: 'What are accordion components?',
+        answer: 'Accordion component are used to generate vertically stacked content in a page, not to be confused with the musical instrument.'
+    },
+    {
+        id : '2',
+        question: 'What are accordion components used for?',
+        answer: 'Accordion component are used to generate vertically stacked content in a page. It is intended to save vertical space by hiding content and reducing scrolling. Accordions contains labels with the relevant information which users can click on to read additional information.'
+    },
+    {
+        id : '3',
+        question: 'What is an example of an accordion component?',
+        answer: 'All of this is a perfect example of an accordion component. By clicking, you are opening and closing the different pieces of the accordion'
+    },
+    {
+        id : '4',
+        question: 'Why use an accordion component?',
+        answer: {
+            example: 'For a better spacial aesthetic',
+            example: 'To save space for less scrolling, and allow the user to only see what they want to see',
+            example: "Because it's just cool!"
+        }
+    },
+    
+]
+
+export default data
