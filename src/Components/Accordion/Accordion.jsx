63e57be4542e976c0834383f697b1bb7664d51e1
Your Name <nellie7190@gmail.com>
2024-02-09 14:12:27 -0500

    single and mult functs working, not connnected to buttons yet

--- src/Components/Accordion/Accordion.jsx
+++ src/Components/Accordion/Accordion.jsx
@@ -1,42 +1,66 @@
-import React, { useState } from 'react'
-import data from './data'
-import './styles.css'
+import React, { useState } from "react";
+import data from "./data";
+import "./styles.css";
 
 const Accordion = () => {
-  const [ selected, setSelected ] = useState(null)
+  const [selected, setSelected] = useState([]);
+  const [enMultSel, setEnMultSel] = useState([])
+  const [multButton, setMultButton] = useState(false)
+  
 
-  function handleSingleSel(id, item) {
+  function handleSingleSel(id) {
     // console.log(item.answer)
-    setSelected(id === selected ? null : id)
+    setSelected(id === selected ? null : id);
   }
 
-  return (
-    <div className='wrapper'>
-      <div className='accordion'>
-      {
-        // Just in case there is no data
-        data && data.length > 0 ? 
-        data.map(item => (
-        <div className='item' key={item.id} >
-          {/* anon funct so it doesn't invoke on pageload */}
-          <div className='title' onClick={()=>handleSingleSel(item.id, item)}>
-            <h3>
-              {item.question}
-              {/* want to change below to be expandable and collapsible */}
-            </h3>
-            <span>+</span> 
-            {
-              selected === item.id && selected ? <div className='content' >{item.answer}</div> : null
-            }
-          </div>
-        </div>
-        ))
-        : <div> No data found </div>
-      }
+  function handleButtonChange() {
+    let oppBtn  = multButton
+    console.log(!oppBtn)
+    return setMultButton(!oppBtn)
+  }
 
+  // function()
+
+  function handleMultSel(id) {
+    console.log(id)
+    let selCopy = selected
+    setSelected(id === selected ? null : [...selCopy, id]);
+    console.log(selected)
+    //  let multSels = selected
+    //  console.log(`This is ${multSels}`)
+    //  multSels.push(id)
+    //  console.log(`This is ${multSels}`)
+    // setEnMultSel(multSels)
+  }
+  return (
+    <div className="wrapper">
+      <button onClick={() => handleButtonChange()}>Enable MultiSelection</button>
+      <div className="accordion">
+        {
+          // Just in case there is no data
+          data && data.length > 0 ? (
+            data.map((item) => (
+              <div className="item" key={item.id}>
+                {/* anon funct so it doesn't invoke on pageload */}
+                <div className="title" onClick={() => handleMultSel(item.id)}>
+                  <h3>
+                    {item.question}
+                    {/* want to change below to be expandable and collapsible */}
+                  </h3>
+                  <span>+</span>
+                  {selected.includes(item.id) ? (
+                    <div className="content">{item.answer}</div>
+                  ) : null}
+                </div>
+              </div>
+            ))
+          ) : (
+            <div> No data found </div>
+          )
+        }
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Accordion
+export default Accordion;
