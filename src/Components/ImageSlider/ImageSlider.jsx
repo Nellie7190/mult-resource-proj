dbb834633db0f10433c4c6662da0a4d93b20e3b6
Your Name <nellie7190@gmail.com>
2024-02-28 23:17:32 -0500

    test

--- src/Components/ImageSlider/ImageSlider.jsx
+++ src/Components/ImageSlider/ImageSlider.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { SliderData } from "./data";
 import "./styles.css";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 
 const ImageSlider = () => {
   console.log(SliderData);
   return (
     <div>
       {SliderData.map((img, idx) => (
-        <img src={img.image} alt={img.image} className="images"/>
+        <img src={img.image} alt={img.image} key={idx} className="images"/>
       ))}
     </div>
   );
